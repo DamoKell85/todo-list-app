4180dbf945b16cee2403f1f1112e3e9c0ef19eb7
Damien Kelly <damien@joulica.io>
2023-07-15 16:38:06 +0100

    Implement TodoListService

--- src/app/services/todo/todo.service.ts
+++ src/app/services/todo/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { TodoItem } from '../../models/todo-item';
+import { Task } from '../../models/task';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +8,39 @@ export class TodoService {
 
   constructor() { }
 
-  public getAllTodoListItems(): TodoItem[] {
-    const todoItemList: TodoItem[] = JSON.parse(localStorage.getItem('tasks') as string);
-    return todoItemList;
+  private readonly storageKey = 'todo-list';
+
+  getTasks(): Task[] {
+    const tasksJson = localStorage.getItem(this.storageKey);
+    return tasksJson ? JSON.parse(tasksJson) : [];
+  }
+
+  saveTasks(tasks: Task[]): void {
+    const tasksJson = JSON.stringify(tasks);
+    localStorage.setItem(this.storageKey, tasksJson);
+  }
+
+  addTask(task: Task): void {
+    const tasks = this.getTasks();
+    tasks.push(task);
+    this.saveTasks(tasks);
+  }
+
+  updateTask(task: Task): void {
+    const tasks = this.getTasks();
+    const index = tasks.findIndex(t => t.id === task.id);
+    if (index !== -1) {
+      tasks[index] = task;
+      this.saveTasks(tasks);
+    }
+  }
+
+  deleteTask(task: Task): void {
+    const tasks = this.getTasks();
+    const index = tasks.findIndex(t => t.id === task.id);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+      this.saveTasks(tasks);
+    }
   }
 }
