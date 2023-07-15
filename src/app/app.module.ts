16cd218843b1dfdcc782cb406b9a8684149e0c47
Damien Kelly <damien@joulica.io>
2023-07-15 16:38:40 +0100

    Adding Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoService } from './services/todo/todo.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { TodoItemComponent } from './components/todo-item/todo-item.component';
     TodoItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
