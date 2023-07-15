221a1d8040ad148b938c78eeadc48bb11d36d539
Damien Kelly <damien@joulica.io>
2023-07-15 16:37:37 +0100

    mplement TodoItemComponent and related components

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../models/task';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
-export class TodoItemComponent implements OnInit {
+export class TodoItemComponent {
+  @Input() task: Task | undefined;
+  @Output() update: EventEmitter<Task> = new EventEmitter<Task>();
+  @Output() delete: EventEmitter<Task> = new EventEmitter<Task>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onStatusChange(): void {
+    this.update.emit(this.task);
   }
 
+  onDelete(): void {
+    this.delete.emit(this.task);
+  }
 }
