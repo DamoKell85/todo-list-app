59a2b252922c020912fc65c086d3b7b4480b4e22
Damien Kelly <damien@joulica.io>
2023-07-15 18:23:56 +0100

    Example Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: [],
+  darkMode: false,
   theme: {
+    extend: {
+      colors: {
+        primary: '#FF3366', // Set your primary color
+        secondary: '#0099FF', // Set your secondary color
+      },
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'], // Customize the font family
+      },
+      boxShadow: {
+        md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)', // Customize the shadow
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
-
+};
